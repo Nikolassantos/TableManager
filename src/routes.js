249cc01bb0923aef2ae5c5ce798fc05f2cfda445
Nikolas dos Santos <58566087+Nikolassantos@users.noreply.github.com>
2020-03-13 21:32:08 -0300

    Emplementação do Drawer de Navegação
    Adicionando Drawer de Navegação e suas dependencias

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer} from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import Home from './Pages/Home';
 
 const Stack = createStackNavigator();
 const bgHeaderColor = '#EE2121';
 const txtHeaderColor = '#fff';
+
+const Drawer = createDrawerNavigator();
 
 function App() { 
   
@@ -31,7 +33,12 @@ function App() {
           name="Home" 
           component={Home} />
   
-        </Stack.Navigator>        
+        </Stack.Navigator>
+
+        <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home}/>
+      </Drawer.Navigator>
+
       </NavigationContainer>
     )
   }
